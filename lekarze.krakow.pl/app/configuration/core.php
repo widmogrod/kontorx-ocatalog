73b8a461f16b411c7b9e658c95c0a7d04d57064d
widmogrod <widmogrod@bd9183ce-cd01-11de-b23d-8995391491c2>
2009-12-03 09:55:13 +0000

    Aktualizacja zmiennych core.php, aktualizacja router.ini, aktualizacja JS dla poszczególnych szablonów catalogów (teraz są uniwersalne skrypty)

--- lekarze.krakow.pl/app/configuration/core.php
+++ lekarze.krakow.pl/app/configuration/core.php
@@ -39,6 +39,8 @@ $catalogOptions = array(
 	'newlekarze' => array(
 		'production' => array(
 			'CATALOG_HOSTNAME'  => 'lekarze.krakow.pl',
+			'CATALOG_SHOW_ROUTE'  => 'lekarz',
+			'CATALOG_LIST_ROUTE'  => 'lekarze',
 			'G_MAP_KEY' => 'ABQIAAAAnCqO9l1WMOgTCJlg9kVlMRRofd7-qOKxoz0eHYyiKpxi9tWtWxR6Y2aP5q7T7h_yT-IMKv4p15b3FA',
 			'ReCAPTCHA_PUBLIC_KEY' => '6LddOwgAAAAAAC380KPzvm2MkQSOUVMpMzJZbX7-',
 			'ReCAPTCHA_PRIVATE_KEY' => '6LddOwgAAAAAAPMbo19cuS5ZBG2OvTOHyS-6CQsV'
@@ -46,6 +48,8 @@ $catalogOptions = array(
 		'development' => array(
 			// http://lekarze.krakow.lh
 			'CATALOG_HOSTNAME'  => 'lekarze.krakow.lh',
+			'CATALOG_SHOW_ROUTE'  => 'lekarz',
+			'CATALOG_LIST_ROUTE'  => 'lekarze',
 			'G_MAP_KEY' => 'ABQIAAAAnCqO9l1WMOgTCJlg9kVlMRRy31BfvwgWWIRKeGy_HOv1ENUTOhTmnFdE9LntmoLBQlfpJ93rZqvJRQ',
 			'ReCAPTCHA_PUBLIC_KEY' => '6LcWwAkAAAAAAJBSCOr3ctqYHEy2PLJxP2a2OFEe',
 			'ReCAPTCHA_PRIVATE_KEY' => '6LcWwAkAAAAAANzH-UDrJtbyuVHBo6Q7sAp94C1u'
@@ -54,6 +58,8 @@ $catalogOptions = array(
 	'newstomatolodzy' => array(
 		'production' => array(
 			'CATALOG_HOSTNAME'  => 'stomatolodzy.krakow.pl',
+			'CATALOG_SHOW_ROUTE'  => 'stomatolog',
+			'CATALOG_LIST_ROUTE'  => 'stomatolodzy',
 			'G_MAP_KEY' => 'ABQIAAAAnCqO9l1WMOgTCJlg9kVlMRScd4uuoniCqHZk8TRh2rI1gQbdrBSg5tTOx4YcGCXRk0Q_bCKoUGwr2Q',
 			'ReCAPTCHA_PUBLIC_KEY' => '6Ld8MggAAAAAAGnhJiTdAEaylMic50irKkVjS77B',
 			'ReCAPTCHA_PRIVATE_KEY' => '6Ld8MggAAAAAAGnhJiTdAEaylMic50irKkVjS77B'
@@ -61,6 +67,8 @@ $catalogOptions = array(
 		'development' => array(
 			// http://stomatolodzy.krakow.lh
 			'CATALOG_HOSTNAME'  => 'stomatolodzy.krakow.lh',
+			'CATALOG_SHOW_ROUTE'  => 'stomatolog',
+			'CATALOG_LIST_ROUTE'  => 'stomatolodzy',
 			'G_MAP_KEY' => 'ABQIAAAAnCqO9l1WMOgTCJlg9kVlMRQldNxPkYdYxKRCRmM_80kVTahxvRQdvL1bkbtpmSHPq0D_TlqM332WCg',
 			'ReCAPTCHA_PUBLIC_KEY' => '',
 			'ReCAPTCHA_PRIVATE_KEY' => ''
@@ -70,6 +78,8 @@ $catalogOptions = array(
 		'production' => array(
 			// http://weterynarze.krakow.pl
 			'CATALOG_HOSTNAME'  => 'weterynarze.krakow.pl',
+			'CATALOG_SHOW_ROUTE'  => 'weterynarz',
+			'CATALOG_LIST_ROUTE'  => 'weterynarze',
 			'G_MAP_KEY' => '',
 			'ReCAPTCHA_PUBLIC_KEY' => '',
 			'ReCAPTCHA_PRIVATE_KEY' => ''
@@ -77,6 +87,8 @@ $catalogOptions = array(
 		'development' => array(
 			// http://weterynarze.krakow.lh
 			'CATALOG_HOSTNAME'  => 'weterynarze.krakow.lh',
+			'CATALOG_SHOW_ROUTE'  => 'weterynarz',
+			'CATALOG_LIST_ROUTE'  => 'weterynarze',
 			'G_MAP_KEY' => 'ABQIAAAAnCqO9l1WMOgTCJlg9kVlMRS3zPBMZjraUIlNdh4fHyQGaVbhMBTmNckxgki84KmNaN9kka5-ii5QAA',
 			'ReCAPTCHA_PUBLIC_KEY' => '',
 			'ReCAPTCHA_PRIVATE_KEY' => ''
@@ -86,6 +98,8 @@ $catalogOptions = array(
 		'production' => array(
 			// http://fryzjerzy.krakow.pl
 			'CATALOG_HOSTNAME'  => 'fryzjerzy.krakow.pl',
+			'CATALOG_SHOW_ROUTE'  => 'fryzjer',
+			'CATALOG_LIST_ROUTE'  => 'fryzjerzy',
 			'G_MAP_KEY' => '',
 			'ReCAPTCHA_PUBLIC_KEY' => '',
 			'ReCAPTCHA_PRIVATE_KEY' => ''
@@ -93,6 +107,8 @@ $catalogOptions = array(
 		'development' => array(
 			// http://fryzjerzy.krakow.lh
 			'CATALOG_HOSTNAME'  => 'fryzjerzy.krakow.lh',
+			'CATALOG_SHOW_ROUTE'  => 'fryzjer',
+			'CATALOG_LIST_ROUTE'  => 'fryzjerzy',
 			'G_MAP_KEY' => 'ABQIAAAAnCqO9l1WMOgTCJlg9kVlMRSx_NKFk20_0wSkl09hHlp1lX2XFBRi_BIT4TykGjX7IQxnGqFj7okcgA',
 			'ReCAPTCHA_PUBLIC_KEY' => '',
 			'ReCAPTCHA_PRIVATE_KEY' => ''
@@ -112,7 +128,17 @@ if (isset($catalogOptions[CATALOG_TYPE])
 	// definiowanie nazwy strony internetowej (wykorzystuje router.ini)
 	defined('CATALOG_HOSTNAME')
 		|| define('CATALOG_HOSTNAME', $catalogOptions['CATALOG_HOSTNAME']);
+
+	// definiowanie nazwy dla router.ini i konfiguracji JS
+	// pokaż wizytówkę "catalog-show"
+	defined('CATALOG_SHOW_ROUTE')
+		|| define('CATALOG_SHOW_ROUTE', $catalogOptions['CATALOG_SHOW_ROUTE']);
+	// pokaz listę/spis "catalog-category"
+	defined('CATALOG_LIST_ROUTE')
+		|| define('CATALOG_LIST_ROUTE', $catalogOptions['CATALOG_LIST_ROUTE']);
 	
+		
+		
 	// API key dla GMaps
 	defined('G_MAP_KEY')
 		|| define('G_MAP_KEY', $catalogOptions['G_MAP_KEY']);
