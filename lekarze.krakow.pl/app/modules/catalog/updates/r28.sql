1cd172e4a4574149ab7f8f6a4536a895839ba867
widmogrod <widmogrod@bd9183ce-cd01-11de-b23d-8995391491c2>
2009-12-07 12:32:30 +0000

    Aktualizacja katalogu + dodanie update 28 (dodaje do tabeli: catalog_district , dzielice krakowa)

---
+++ lekarze.krakow.pl/app/modules/catalog/updates/r28.sql
@@ -0,0 +1,25 @@
+-- 
+-- Zrzut danych tabeli `catalog_district`
+-- Dzielnice Krakowa
+-- 
+
+INSERT INTO `catalog_district` (`id`, `path`, `name`, `url`, `lat`, `lng`, `visible`, `meta_title`, `meta_description`, `meta_keywords`) VALUES (3, '5', 'Stare Miasto <em>I</em>', 'krakow-stare-miasto', 0.000000, 0.000000, 0, 'KrakÃ³w, Stare Miasto (Dzielnica 1)', 'Stomatolog Stare Miasto (dzielnica 1), KrakÃ³w ', 'stomatolog,dentysta,stare miasto,dzielnica 1,dzielnica i,krakÃ³w'),
+(4, '5', 'KrakÃ³w GrzegÃ³rzki <em>II</em>', 'krakow-grzegorzki', 0.000000, 0.000000, 0, 'KrakÃ³w GrzegÃ³rzki (Dzielnica II)', 'Stomatolog KrakÃ³w GrzegÃ³rzki (Dzielnica II)', 'stomatolog,dentysta,krakÃ³w,grzegÃ³rzki,dzielnica 1,dzielnica ii'),
+(5, '', 'KrakÃ³w', 'krakow', 0.000000, 0.000000, 0, 'KrakÃ³w', '', ''),
+(6, '5', 'PrÄ…dnik Czerwony <em>III</em>', 'krakow-pradnik-czerwony', 0.000000, 0.000000, 0, 'PrÄ…dnik Czerwony Dzielnica III', 'Stomatolog, dentysta, KrakÃ³w, PrÄ…dnik Czerwony, Dzielnica III', 'PrÄ…dnik czerwony, dzielnica,trzecia,3,III,KrakÃ³w,stomatolog,dentysta'),
+(7, '5', 'PrÄ…dnik BiaÅ‚y <em>IV</em>', 'krakow-pradnik-bialy', 0.000000, 0.000000, 0, 'KrakÃ³w, PrÄ…dnik BiaÅ‚y, Dzielnica IV', 'Stomatolog, dentysta, KrakÃ³w, PrÄ…dnik BiaÅ‚y, Dzielnica IV', 'PrÄ…dnik BiaÅ‚y, dzielina,czwarta,4,IV,krakÃ³w,stomatolog,dentysta'),
+(8, '5', 'Krowodrza/ÅobzÃ³w <em>V</em>', 'krakow-krowodrza-lobzow', 0.000000, 0.000000, 0, 'Krowodrza Dzielnica V', 'Stomatolog, dentysta, KrakÃ³w, Krowodrza, Dzielnica V', 'Krowodrza, dzielina,piÄ…ta,5,V,krakÃ³w,stomatolog,dentysta'),
+(9, '5', 'Bronowice <em>VI</em>', 'krakow-bronowice', 0.000000, 0.000000, 0, 'Bronowice Dzielnica VI', 'Stomatolog, dentysta, KrakÃ³w, Bronowice, Dzielnica VI', 'Bronowice, dzielnica,szÃ³sta,6,VI,KrakÃ³w,stomatolog,dentysta'),
+(10, '5', 'Zwierzyniec <em>III</em>', 'krakow-zwierzyniec', 0.000000, 0.000000, 0, 'Zwierzyniec Dzielnica VII', 'Stomatolog, dentysta, KrakÃ³w, PrÄ…dnik Czerwony, Dzielnica III', 'Zwierzyniec, dzielnica,siudma,7,VII,KrakÃ³w,stomatolog,dentysta'),
+(11, '5', 'DÄ™bniki <em>VIII</em>', 'krakow-debniki', 0.000000, 0.000000, 0, 'DÄ™bniki Dzielnica VIII', 'Stomatolog, dentysta, KrakÃ³w, DÄ™bniki, Dzielnica VIII', 'DÄ™bniki,dzielnica,Ã³sma,8,VIII,KrakÃ³w,stomatolog,dentysta'),
+(12, '5', 'Åagiewniki-Borek FaÅ‚Ä™cki <em>IX</em>', 'krakow-lagiewniki-borek-falecki', 0.000000, 0.000000, 0, 'Åagiewniki-Borek FaÅ‚Ä™cki Dzielnica IX', 'Stomatolog, dentysta, KrakÃ³w, Åagiewniki-Borek FaÅ‚Ä™cki, Dzielnica IX', 'Åagiewniki,Borek FaÅ‚Ä™cki, dzielnica,dziewiÄ…ta,9,IX,KrakÃ³w,stomatolog,dentysta'),
+(13, '5', 'Swoszowice <em>X</em>', 'krakow-swoszowice', 0.000000, 0.000000, 0, 'Swoszowice Dzielnica IX', 'Stomatolog, dentysta, KrakÃ³w, Swoszowice, Dzielnica X', 'Swoszowice, dzielnica,dziesiÄ…ta,10,X,KrakÃ³w,stomatolog,dentysta'),
+(14, '5', 'PodgÃ³rze Duchackie <em>XI</em>', 'krakow-podgorze-duchackie', 0.000000, 0.000000, 0, 'PodgÃ³rze Duchackie Dzielnica XI', 'Stomatolog, dentysta, KrakÃ³w, PodgÃ³rze Duchackie, Dzielnica XI', 'PodgÃ³rze Duchackie, dzielnica,jedenasta,11,XI,KrakÃ³w,stomatolog,dentysta'),
+(15, '5', 'BieÅ¼anÃ³w-Prokocim <em>XII</em>', 'krakow-biezanow-prokocim', 0.000000, 0.000000, 0, 'BieÅ¼anÃ³w-Prokocim Dzielnica XII', 'Stomatolog, dentysta, KrakÃ³w,BieÅ¼anÃ³w-Prokocim, Dzielnica XII', 'BieÅ¼anÃ³w-Prokocim, dzielnica,dwunasta,12,XII,KrakÃ³w,stomatolog,dentysta'),
+(16, '5', 'PodgÃ³rze <em>XIII</em>', 'krakow-podgorze', 0.000000, 0.000000, 0, 'PodgÃ³rze Dzielnica XIII', 'Stomatolog, dentysta, PodgÃ³rze, Dzielnica XIII', 'PodgÃ³rze, dzielnica,trzynasta,13,XIII,KrakÃ³w,stomatolog,dentysta'),
+(17, '5', 'CzyÅ¼yny <em>XIV</em>', 'krakow-czyzyny', 0.000000, 0.000000, 0, 'CzyÅ¼yny Dzielnica XIV', 'Stomatolog, dentysta, KrakÃ³w,CzyÅ¼yny, Dzielnica XIV', 'CzyÅ¼yny, dzielnica,czternasta,14,XIV,KrakÃ³w,stomatolog,dentysta'),
+(18, '5', 'Mistrzejowice <em>XV</em>', 'krakow-mistrzejowice', 0.000000, 0.000000, 0, 'Mistrzejowice Dzielnica XV', 'Stomatolog, dentysta, KrakÃ³w,Mistrzejowice, Dzielnica XV', 'Mistrzejowice, dzielnica,pietnasta,15,XV,KrakÃ³w,stomatolog,dentysta'),
+(19, '5', 'BieÅ„czyce <em>XVI</em>', 'krakow-bienczyce', 0.000000, 0.000000, 0, 'BieÅ„czyce Dzielnica XVI', 'Stomatolog, dentysta, KrakÃ³w,BieÅ„czyce, Dzielnica XVI', 'BieÅ„czyce, dzielnica,szesnasta,16,XVI,KrakÃ³w,stomatolog,dentysta'),
+(20, '5', 'WzgÃ³rza KrzesÅ‚awickie <em>XVII</em>', 'krakow-wzgorza-krzeslawickie', 0.000000, 0.000000, 0, 'WzgÃ³rza KrzesÅ‚awickie Dzielnica XVII', 'Stomatolog, dentysta, KrakÃ³w,WzgÃ³rza KrzesÅ‚awickie, Dzielnica XVII', 'WzgÃ³rza KrzesÅ‚awickie, dzielnica,pietnasta,17,XVII,KrakÃ³w,stomatolog,dentysta'),
+(21, '5', 'Nowa Huta <em>XVIII</em>', 'krakow-nowa-huta', 0.000000, 0.000000, 0, 'Nowa Huta Dzielnica XVIII', 'Stomatolog, dentysta, KrakÃ³w,Nowa Huta, Dzielnica XVIII', 'Nowa Huta, dzielnica,osiemnasta,18,XVIII,KrakÃ³w,stomatolog,dentysta');
+
