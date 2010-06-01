970e38cd2c038f09c1ca37ddd3060d29509dbb36
widmogrod <widmogrod@bd9183ce-cd01-11de-b23d-8995391491c2>
2010-06-01 15:01:06 +0000

    aktualizacja text2html

--- lekarze.krakow.pl/app/modules/catalog/views/helpers/Text2html.php
+++ lekarze.krakow.pl/app/modules/catalog/views/helpers/Text2html.php
@@ -14,7 +14,7 @@ class Catalog_View_Helper_Text2html extends Zend_View_Helper_Abstract {
 	public function text2html($text) {
 		// tworzy listę <ul><li>
 		$text = preg_replace(
-			'#\n+\-\s*([^\n\r]+)[\n\r]*#i','<li>$1</li>',
+			'#\n*\-\s*([^\n]+)#i','<li>$1</li>',
 			$text);
 		$text = preg_replace(
 			'#(<li>(.*)</li>)#i','<ul>$1</ul>', $text);
