57e03cbd98a1b8abbbb47cf790663ba13df667e9
widmogrod <widmogrod@bd9183ce-cd01-11de-b23d-8995391491c2>
2010-06-17 08:51:48 +0000

    aktualizacja systemu parsowania treści do markdown

--- lekarze.krakow.pl/app/modules/catalog/views/helpers/Text2html.php
+++ lekarze.krakow.pl/app/modules/catalog/views/helpers/Text2html.php
@@ -1,4 +1,6 @@
 <?php
+include_once "markdown.php";
+
 /**
  * @author gabriel
  *
@@ -8,16 +10,17 @@ class Catalog_View_Helper_Text2html extends Zend_View_Helper_Abstract {
 	/**
 	 * @param string $text
 	 * @return string
-	 * 
-	 * @todo A co jesli w tekście są inne myślniki?
 	 */
 	public function text2html($text) {
-		// tworzy listę <ul><li>
-		$text = preg_replace(
-			'#\n*\-\s*([^\n]+)#i','<li>$1</li>',
-			$text);
-		$text = preg_replace(
-			'#(<li>(.*)</li>)#i','<ul>$1</ul>', $text);
+		
+		$text = Markdown($text);
+		
+//		// tworzy listę <ul><li>
+//		$text = preg_replace(
+//			'#\n*\-\s*([^\n]+)#i','<li>$1</li>',
+//			$text);
+//		$text = preg_replace(
+//			'#(<li>(.*)</li>)#i','<ul>$1</ul>', $text);
 		
 		print $text;
 	}
